f180d00ea550e59aaa2f7b046c98eba3a02a622a
shawngoga <smn224@lehigh.edu>
2022-06-10 13:55:44 -0400

    second commit

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
 let port  = process.env.PORT || 3000;
-const importData = require("./data.json");
+//const importData = require("./data.json");
 
 app.get("/" , (req , res ) => {
     res.send("Hello WORLD")
 });
 
-app.get("/players" , (req , res) =>{
+/*app.get("/players" , (req , res) =>{
     res.send(importData);
 }
-);
+); */
 
 app.listen(port, () =>{
 console.log('Example app is listening on port http://localhost:3000');
